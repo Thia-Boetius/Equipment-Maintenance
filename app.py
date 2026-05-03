a4278a879d20661e5dff9fdf99b8f4143877d9b7
radj554 <gamingwithradj@gmail.com>
2026-05-03 11:54:53 -0300

    initializing database

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "my_secret_key"
 
+#configure sql alchemy 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///user.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+#database Model
+
+
+
+
+
+
+
+
+
+
+#routes
 @app.route("/")
 def home():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
     return render_template("home.html")
 
 @app.route("/login")
@@ -15,4 +39,6 @@ def register():
     return render_template('register.html')
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
